c6a5d6ad685711cb5a0049f44d964f8b9d09319a
rafael-quarezemin <rafael.quarezemin@gmail.com>
2021-11-26 15:25:10 -0400

    Exercício de ciclo de vida realizado com dificuldades - não está 100%

--- modulo3/ciclo-vida-todo/boiler-plate/src/App.js
+++ modulo3/ciclo-vida-todo/boiler-plate/src/App.js
@@ -9,7 +9,7 @@ const TarefaList = styled.ul`
 
 const Tarefa = styled.li`
   text-align: left;
-  text-decoration: ${({completa}) => (completa ? 'line-through' : 'none')};
+  text-decoration: ${({ completa }) => (completa ? 'line-through' : 'none')};
 `
 
 const InputsContainer = styled.div`
@@ -19,47 +19,87 @@ const InputsContainer = styled.div`
 `
 
 class App extends React.Component {
-    state = {
-      tarefas: [ {
-        id: Date.now(),
-        texto: 'Texto da primeira tarefa',
-        completa: false
-      }, {
-        id: Date.now(),
-        texto: 'Texto da segunda tarefa',
-        completa: true
-      } ],
+  state = {
+    tarefas: [{
+      id: Date.now(),
+      texto: 'Texto da primeira tarefa',
+      completa: false
+    }, {
+      id: Date.now(),
+      texto: 'Texto da segunda tarefa',
+      completa: true
+    }],
 
-      inputValue: '',
-      
-      filtro: ''
+    inputValue: '',
+
+    filtro: '',
+
+    listaDeTarefas: []
 
 
 
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.texto !== this.state.texto) {
+      localStorage.setItem('texto', this.state.texto)
     }
-
-  componentDidUpdate() {
 
   };
 
   componentDidMount() {
+    this.pegarTarefas()
+    const textoLS = localStorage.getItem('texto') || ''
 
+    this.setState({ texto: textoLS })
   };
 
   onChangeInput = (event) => {
-
+    this.setState({ inputValue: event.target.value })
   }
 
   criaTarefa = () => {
+    const novaTarefa = {
+      id: Date.now(),
+      texto: this.state.inputValue,
+      completa: false
 
+    }
+    const novaListaDeTarefa = [... this.state.tarefas, novaTarefa]
+
+    // 1-Salvando no localStorage
+    localStorage.setItem('históricoTarefas', JSON.stringify(novaListaDeTarefa))
+
+    this.setState({ tarefas: novaListaDeTarefa })
+    this.setState({ inputValue: '' })
   }
 
   selectTarefa = (id) => {
-
+    const novaListaDeTarefa = this.state.tarefas.map((post) => {
+      if (id === post.id) {
+        const novaTarefa = {
+          ...post,
+          completa: !post.completa
+        }
+        return novaTarefa
+      } else {
+        return post
+      }
+    })
+    this.setState({ tarefas: novaListaDeTarefa })
   }
 
   onChangeFilter = (event) => {
+    this.setState({ filter: event.target.value })
+  }
 
+  guardarTarefas = () => {
+
+  }
+
+  pegarTarefas = () => {
+    const tarefasSalvasLS = JSON.parse(localStorage.getItem('historicoTarefas')) || []
+    this.setState({ listaDeTarefas: tarefasSalvasLS })
   }
 
   render() {
@@ -78,10 +118,12 @@ class App extends React.Component {
       <div className="App">
         <h1>Lista de tarefas</h1>
         <InputsContainer>
-          <input value={this.state.inputValue} onChange={this.onChangeInput}/>
+          <input
+            value={this.state.inputValue}
+            onChange={this.onChangeInput} />
           <button onClick={this.criaTarefa}>Adicionar</button>
         </InputsContainer>
-        <br/>
+        <br />
 
         <InputsContainer>
           <label>Filtro</label>
